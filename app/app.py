fa0f0b1759372b009f5cecd4ce7c4b36a230af36
Steven Lai <laisteve17@gmail.com>
2025-07-20 16:42:21 -0700

    race condition db flask

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import os
-
+import time
 
 import psycopg
 from psycopg.rows import dict_row
@@ -8,27 +8,33 @@ from psycopg.rows import dict_row
 # Database connection settings
 DB_HOST = "db"
 DB_PORT = 5432
-DB_NAME = "db"
+DB_NAME = "caro"
 DB_USER = "alvin"
 DB_PASSWORD = "923401926"
 
-# Connection string
-conn_str = f"host={DB_HOST} port={DB_PORT} dbname={DB_NAME} user={DB_USER} password={DB_PASSWORD}"
+# Wait for DB to start listening
+for _ in range(3):
 
-try:
-    # Connect to the PostgreSQL server
-    with psycopg.connect(conn_str, row_factory=dict_row) as conn:
-        print("Connection successful!")
+    conn_str = f"host={DB_HOST} port={DB_PORT} dbname={DB_NAME} user={DB_USER} password={DB_PASSWORD}"
 
-        # Create a cursor and execute a simple query
-        with conn.cursor() as cur:
-            cur.execute("SELECT version();")
-            version = cur.fetchone()
-            print("PostgreSQL version:", version["version"])
+    try:
+        # Connect to the PostgreSQL server
+        with psycopg.connect(conn_str, row_factory=dict_row) as conn:
+            print("Connection successful!")
 
-except Exception as e:
-    print("Failed to connect to the database:")
-    print(e)
+            # Create a cursor and execute a simple query
+            with conn.cursor() as cur:
+                cur.execute("SELECT version();")
+                version = cur.fetchone()
+                print("PostgreSQL version:", version["version"])
+            
+            break
+
+    except Exception as e:
+        print("Failed to connect to the database:")
+        print(e)
+
+    time.sleep(1)
 
 app = Flask(__name__)
 
